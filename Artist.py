6cc5d8da0cd69df9935c287984ae452250b7ce28
jereme-yang <115114700+jereme-yang@users.noreply.github.com>
2023-04-23 21:54:05 -0400

    data.json

--- Artist.py
+++ Artist.py
@@ -3,7 +3,7 @@
 #artist object
 #popularity and followers stored to calculate the artists' difficulty rating
 class Artist:
-    def __init__(self, id, name, popularity, genres, href, images, followers, top_track_id, related_artist_name_one, related_artist_id_one, related_artist_name_two, related_artist_id_two):
+    def __init__(self, id, name, popularity, genres, href, images, followers):
         self.id = id #used 
         self.name = name #used
         self.popularity = popularity #used
@@ -11,19 +11,13 @@ class Artist:
         self.href = href
         self.images = images
         self.followers = followers #used
-        self.top_track_id = top_track_id
-        self.related_artist_name_one = related_artist_name_one #used
-        self.related_artist_id_one = related_artist_id_one #used
-        self.related_artist_name_two = related_artist_name_two #used 
-        self.related_artist_id_two = related_artist_id_two #used
+
 
     #/==========ACCESSORS==========/
     def get_name(self):
         return self.name
-    def get_related_artist_name_one(self):
-        return self.related_artist_name_one
-    def get_related_artist_name_two(self):
-        return self.related_artist_name_two
+    def get_id(self):
+        return self.id
     def get_obscurity_rating(self): #used in sorting artist objects by difficulty in merge/quick sort
         return self.popularity * self.followers // 100
     
--- main.py
+++ main.py
@@ -2,12 +2,26 @@ from GenerateArtists import generate_artist_list
 from SortingAlgorithms import mergeSort, quickSort
 from time import sleep
 from random import randint
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import sys
+print(sys.getrecursionlimit())
+sys.setrecursionlimit(10000)
+print(sys.getrecursionlimit())
 
 
-artist_list = [] #holds artist objects
-generate_artist_list(artist_list)
+client_id = '024d2accff6645c7919e60466b8ec3f0'
+client_secret = '2ed4211bd77444bf8f04f87508c4073c'
 
+client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+with open("data.json") as file:
+    artist_list = json.load(file)
+artist_list = generate_artist_list(artist_list)
 #organize data
+
 sortingAlg = ""
 while sortingAlg != '1' and sortingAlg != '2':
 
@@ -23,24 +37,29 @@ while sortingAlg != '1' and sortingAlg != '2':
 score = 0
 level = 0
 has_lost = False
-current_index = len(artist_list)-1
+current_index = len(artist_list) - randint(1, 100)
 while (not has_lost):
     current_artist = artist_list[current_index]
+    options = sp.artist_related_artists(current_artist.get_id())['artists'][:2]
+    while len(options) != 2:
+        current_index -= 1
+        current_artist = artist_list[current_index]
+        options = sp.artist_related_artists(current_artist.get_id())['artists'][:2]
     print(f'LEVEL : {level}\n NAME OF ARTIST : {current_artist.get_name()}\n')
-    options = [current_artist.get_related_artist_name_one(), current_artist.get_related_artist_name_two()]
+    
     random_index = randint(0, 1)
     correct_option = "1" if random_index == 0 else "2"
     if (correct_option == "1"):
-        print("Option 1: "+ options[0])
-        print("Option 2: "+ options[1])
+        print("Option 1: "+ options[0]['name'])
+        print("Option 2: "+ options[1]['name'])
     else:
-        print("Option 1: "+ options[1])
-        print("Option 2: "+ options[0])
+        print("Option 1: "+ options[1]['name'])
+        print("Option 2: "+ options[0]['name'])
     user_input = input("type 1 for option 1 and 2 for option 2: ")
     if (user_input == correct_option):
         score += 1
         level += 1
-        current_index -= 1
+        current_index -= randint(1, 100)
     else:
         has_lost = True
         print("you lost! Your final score: " + str(score))
